964a9be73c2381a991c7ad21d21699b4edf6da3a
Prashant Malani <pmalani@google.com>
2015-03-14 13:01:44 -0700

    Load program into memory
    Add debug option in command line.
    
    Next up, use SDL to open up a graphics buffer
    
    Change-Id: Ieebd47bd71c4037c48ad56dfd841187594a22114

--- chip8.c
+++ chip8.c
@@ -10,19 +10,22 @@
 /* Common address locations */
 #define FONTSET_BEGIN		0x50
 #define PROGRAM_BEGIN		0x200
+#define MAX_MEM			0x1000
+#define SCREEN_Y		64
+#define SCREEN_X		32
 
 /* TODO: We should eventually shift this to the main function */
-int debug_level = LOG_DEBUG;
+int debug_level = LOG_INFO;
 
 /* Chip 8 hardware registers and machine state */
 unsigned short opcode;
-unsigned char mem[4096];
+unsigned char mem[MAX_MEM];
 
 unsigned char V[16];
 unsigned short I;
 unsigned short pc;
 
-unsigned char gfx[64 *32];
+unsigned char gfx[SCREEN_X * SCREEN_Y];
 
 unsigned char delay_timer;
 unsigned char sound_timer;
@@ -78,9 +81,9 @@ void initialize()
 	delay_timer = 0;
 	sound_timer = 0;
 
-	// Clear I and PC registers
+	// Initialize PC and I registers
 	I = 0;
-	pc = 0;
+	pc = PROGRAM_BEGIN;
 
 	/* Load fontset into memory */
 	for (i = 0; i < 80; i++)
@@ -90,18 +93,58 @@ void initialize()
 int loadProgram(char *filepath)
 {
 	FILE *fp = NULL;
-	fp = fopen(filepath, O_RDONLY);
+	long int fsize;
+	int i;
+
+	fp = fopen(filepath, "rb");
 	if (!fp) {
 		LOGE("Error opening file %s\n", filepath);
 		return -1;
 	}
+	fseek(fp, 0, SEEK_END);
+	fsize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	if (fsize > MAX_MEM - PROGRAM_BEGIN) {
+		LOGE("Input file is larger than mem capacity\n");
+		fclose(fp);
+		return -1;
+	}
+
+	fread(&mem[PROGRAM_BEGIN], fsize, 1, fp);
 	fclose(fp);
+
+	LOGD("\n");
+	// Print out the program contents for debug purposes
+	for (i = 0; i < fsize; i++) {
+		LOGD("%02x ", mem[PROGRAM_BEGIN + i]);
+		if (!((i+1) % 8))
+			LOGD("\n");
+	}
+	LOGD("\n");
+
 	return 0;
 }
 
-int main()
+void execute()
 {
+	for (;;) {
+		// Fetch opcode
+		opcode = mem[pc] << 8 | mem[pc + 1];
+		LOGD("Fetched opcode is %02x\n", opcode);
+		break;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1) {
+		if (!strcmp(argv[1], "--debug"))
+			debug_level = LOG_DEBUG;
+	}
 	LOGI("Initializing hardware\n");
 	initialize();
+	loadProgram("./zero_demo.ch8");
+	execute();
 	return 0;
 }
